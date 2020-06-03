60f42287c658edcdc9fcb89c58809cc3c40de454
Jessica Latten <jlatten.simon@gmail.com>
2020-06-03 12:26:40 -0500

    text content only running run if else statement

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ singButton.addEventListener("click", function() {
     let friends = [" Jennifer", " Eryka", " Charmaine", " Dommy", " Lurong"];
 
 
-    let a = " lines of code in the file ";
+    let a = " lines of code in the file";
     let b = " lines of code";
     let c = " strikes one out, clears it all out, ";
-    let d = " line of code ";
+    let d = " line of code";
     let e = " no more";
 
 
@@ -18,11 +18,22 @@ singButton.addEventListener("click", function() {
     for (let f = 0; f < friends.length; f++) {
         //console.log(friends[f])
 
+        let friendDiv = document.createElement("div");
+        friendDiv.classList.add("friend");
+        let friendH3 = document.createElement("h3");
+        friendH3.textContent = friends[f];
+        friendDiv.appendChild(friendH3);
+        document.body.appendChild(friendDiv);
+
         for (let n = 99; n > 0; n--) {
-            if (n == 1) {
-                console.log(n + d + "in the file " + n + d + friends[f] + c + e + a);
-            } else {
-                console.log(n + a + n + b + friends[f] + c + a);
+            let friendP = document.createElement("p");
+            friendDiv.appendChild(friendP);
+
+
+            if (n === 1) {
+                friendDiv.textContent = n + a + " " + n + b + ": " + friends[f] + " " + c + n + a;
+            } else if (n === 2) {
+                friendDiv.textContent = n + d + " in the file, " + n + d + ": " + friends[f] + " " + c + e + a + ".";
             }
         }
     }
